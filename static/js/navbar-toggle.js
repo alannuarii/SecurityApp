9eb79d1a90f97914e23768c936ba3d27a64bbde1
alannuarii <alannuariii@gmail.com>
2022-07-28 00:19:24 +0800

    Update

--- static/js/navbar-toggle.js
+++ static/js/navbar-toggle.js
@@ -1,6 +1,8 @@
 const awal = document.querySelector(".navbar-toggler");
+const camera = document.getElementById("camera");
 
 awal.addEventListener("click", (e) => {
   e.preventDefault();
   awal.firstElementChild.classList.toggle("bi-chevron-double-right");
+  camera.classList.toggle("d-none");
 });
